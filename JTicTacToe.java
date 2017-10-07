3081526972347af4c9c6eb23439efc33dade50d3
andre-devito <andre_devito@>
2017-10-07 10:45:37 -0300

    v1.0

--- JTicTacToe.java
+++ JTicTacToe.java
@@ -73,7 +73,7 @@ public class JTicTacToe extends JFrame
 	//********************************************************************************************************************************************
 	
 	//*** Procedimentos de Inicializacao *********************************************************************************************************
-	private void inicializaComponentes() {
+	private void inicializaCmponentes() {
 	
 		//Instanciar os Componentes
 		this.setLayout(null);
